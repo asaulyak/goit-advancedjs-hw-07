ee8ec2acfa863742f20c23093e10812b10b5d016
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-02-03 14:54:02 +0200

    complete tasks 1-4

--- src/3.ts
+++ src/3.ts
@@ -2,10 +2,22 @@
   Ви створюєте гру, де є персонажі з різними ролями.
   Зараз ви працюєте над класом Wizard, який має реалізовувати два інтерфейси - ICharacter та ISpellCaster.
 
-  Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard. 
-  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
+  Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard.
+  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp.
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
+
+export interface ICharacter {
+  name: string;
+  level: number;
+
+  introduce: (phrase: string) => void;
+  levelUp: () => void;
+}
+
+export interface ISpellCaster {
+  castSpell: () => void;
+}
 
 // реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
@@ -34,6 +46,6 @@ const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
 wizard.castSpell();
-wizard.levelUp();  // Level up! New level is 16
+wizard.levelUp(); // Level up! New level is 16
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,47 @@
+class Key {
+  private readonly signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+
+    console.log('tenants', this.tenants);
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +51,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { Key, Person, House, MyHouse };
